aa0911acbe61b8272f5c4736962985d23d8aa587
Kevin Lacker <lacker@gmail.com>
2024-06-20 10:41:16 -0700

    CSA and block

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 # Matching the config for bitstrings
 NUM_LAYERS = 6
 NUM_HEADS = 6
-NUM_EMBED = 384
+EMBED_DIM = 384
 
 
 class MLP(nn.Module):
@@ -25,17 +25,43 @@ class MLP(nn.Module):
         return x
 
 
+class CausalSelfAttention(nn.Module):
+    """
+    Just MultiheadAttention, adding a causal mask.
+    """
+    def __init__(self, embed_dim, num_heads):
+        super().__init__()
+        self.embed_dim = embed_dim
+        self.attention = nn.MultiheadAttention(embed_dim, num_heads)
+
+    def forward(self, x):
+        seq_len, _batch_size, embed_dim = x.size()
+        assert embed_dim == self.embed_dim
+        # Create the causal mask (upper triangular with ones above the diagonal)
+        mask = torch.triu(torch.ones(seq_len, seq_len), diagonal=1).bool().to(x.device)
+        output, _ = self.attention(x, x, x, attn_mask=mask)
+        return output
+
+
 class Block(nn.module):
     """
-    A transformer block.
+    A single transformer block.
     """
-    def __init__(self, num_embed, num_heads):
-        raise "TODO"
+    def __init__(self, embed_dim, num_heads):
+        self.norm1 = nn.LayerNorm(embed_dim)
+        self.csa = CausalSelfAttention(embed_dim, num_heads)
+        self.norm2 = nn.LayerNorm(embed_dim)
+        self.mlp = MLP(embed_dim)
+
+    def forward(self, x):
+        x = x + self.csa(self.norm1(x))
+        x = x + self.mlp(self.norm2(x))
+        return x
 
 
 class DecoderOnly(nn.Module):
     """
-    Decoder only transformer, with embedding layers.
+    Decoder-only transformer, including embedding layers.
     """
     def __init__(self, num_features, num_layers):
         super().__init__()
