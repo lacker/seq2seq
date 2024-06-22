7d574f8652f77d0a55d6d06e13d3912161900029
Kevin Lacker <lacker@gmail.com>
2024-06-21 17:39:36 -0700

    train script looks ok

--- train.py
+++ train.py
@@ -1,6 +1,9 @@
 import data
+import math
 import model
 import numpy as np
+import os
+import time
 import torch
 
 
@@ -8,7 +11,7 @@ eval_interval = 250
 eval_iters = 200
 log_interval = 10
 batch_size = 128
-learning_rate = 1e-3  # with baby networks can afford to go a bit higher
+base_learning_rate = 1e-3  # with baby networks can afford to go a bit higher
 
 max_iters = 100000
 lr_decay_iters = max_iters  # make equal to max_iters usually
@@ -16,6 +19,7 @@ weight_decay = 1e-1
 min_lr = 1e-4  # learning_rate / 10 usually
 beta1 = 0.9
 beta2 = 0.99  # make a bit bigger because number of tokens per iter is small
+grad_clip = 1.0
 
 warmup_iters = 100  # not super necessary potentially
 
@@ -30,6 +34,8 @@ torch.backends.cuda.matmul.allow_tf32 = True
 torch.backends.cudnn.allow_tf32 = True
 ptdtype = torch.bfloat16
 context = torch.amp.autocast(device_type="cuda", dtype=dtype)
+
+checkpoint_path = os.path.join(os.path.dirname(__file__), "checkpoint.pt")
 
 
 # Essentially a data loader
@@ -61,7 +67,7 @@ model = model.DecoderOnly(config)
 model.to("cuda")
 
 optimizer = model.configure_optimizers(
-    weight_decay, learning_rate, (beta1, beta2), "cuda"
+    weight_decay, base_learning_rate, (beta1, beta2), "cuda"
 )
 
 print("compiling the model...")
@@ -82,3 +88,65 @@ def estimate_loss():
             losses[k] = loss.item()
         answer[i] = losses.mean()
     return tuple(answer)
+
+
+def get_learning_rate(i):
+    "Get the learning rate to use for the ith iteration."
+    if i < warmup_iters:
+        return base_learning_rate * i / warmup_iters
+    elif i > lr_decay_iters:
+        return min_lr
+
+    # use "cosine decay"
+    decay_ratio = (i - warmup_iters) / (lr_decay_iters - warmup_iters)
+    assert 0 <= decay_ratio <= 1
+    coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio))
+    assert 0 <= coeff <= 1
+    return min_lr + (base_learning_rate - min_lr) * coeff
+
+
+# The training loop.
+# Start by fetching the very first batch.
+x, y = get_batch(encoding.train)
+current_time = time.time()
+for step in range(max_iters):
+    # Determine the learning rate for this iteration
+    lr = get_learning_rate(step)
+    for group in optimizer.param_groups:
+        group["lr"] = lr
+
+    # Evaluate the loss
+    if step % eval_interval == 0:
+        train_loss, val_loss = estimate_loss()
+        print(f"{step=} {train_loss=:.3f} {val_loss=:.3f}")
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+            print("saving checkpoint...")
+            checkpoint = {
+                "model": model.state_dict(),
+                "optimizer": optimizer.state_dict(),
+                "step": step,
+                "best_val_loss": best_val_loss,
+                "config": config,
+            }
+            torch.save(checkpoint, checkpoint_path)
+
+    # Forward pass
+    with context:
+        logits, loss = model(x, y)
+    # Should async prefetch
+    x, y = get_batch(encoding.train)
+    # Backward pass
+    loss.backward()
+    torch.nn.utils.clip_grad_norm_(model.parameters(), grad_clip)
+    # Flush gradients to free up memory
+    optimizer.zero_grad(set_to_none=True)
+
+    # Timing and logging
+    next_time = time.time()
+    batch_time = next_time - current_time
+    current_time = next_time
+    if step % log_interval == 0:
+        # Note: this is a CPU-GPU sync point.
+        batch_loss = loss.item()
+        print(f"{step=} {batch_loss=:.3f} {batch_time=:.3f}s")
